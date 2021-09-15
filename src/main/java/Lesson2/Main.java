f4985c83d5aa476e0dd655e334a2b016bc5c323f
Alexandr <docalex@mail.ru>
2021-09-15 20:48:58 +0400

    Lesson2 Task1-3 done

--- src/main/java/Lesson2/Main.java
+++ src/main/java/Lesson2/Main.java
@@ -9,6 +9,32 @@ import java.util.function.UnaryOperator;
 
 public class Main {
     public static void main(String[] args) {
+        int n = 100000;
+        int item;
+        Random random = new Random();
+        MyArrayList<Integer> list1 = new MyArrayList<>(n);
+        MyArrayList<Integer> list2 = new MyArrayList<>(n);
+        MyArrayList<Integer> list3 = new MyArrayList<>(n);
+
+        for (int i = 0; i < n; i++) {
+            item = random.nextInt(100);
+            list1.add(item);
+            list2.add(item);
+            list3.add(item);
+        }
+
+        long startTime = System.currentTimeMillis();
+
+        list1.selectionSort();
+        System.out.println("Selection Sort time - " + (System.currentTimeMillis() - startTime) / 1000 + "sec");
+        startTime = System.currentTimeMillis();
+
+        list2.insertionSort();
+        System.out.println("Insertion Sort time - " + (System.currentTimeMillis() - startTime) / 1000 + "sec");
+        startTime = System.currentTimeMillis();
+
+        list3.bubbleSort();
+        System.out.println("Bubble Sort time - " + (System.currentTimeMillis() - startTime) / 1000 + "sec");
 
 
     }
