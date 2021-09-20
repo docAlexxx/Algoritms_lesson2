363a4dc6af1cde1edcbeac4bc1e0690f44ea31ff
Alexandr <docalex@mail.ru>
2021-09-20 17:07:06 +0400

    Lesson3 Class MyDequeue added

--- src/main/java/Lesson3/Main.java
+++ src/main/java/Lesson3/Main.java
@@ -29,16 +29,34 @@ public class Main {
 //            System.out.println(queue.remove());
 //        }
 
-        MyPriorityQueue<Integer> mpq = new MyPriorityQueue<>();
+        //       MyPriorityQueue<Integer> mpq = new MyPriorityQueue<>();
 
-        mpq.insert(5);
-        mpq.insert(2);
-        mpq.insert(20);
-        mpq.insert(21);
-        mpq.insert(4);
+        //       mpq.insert(5);
+        //       mpq.insert(2);
+        //       mpq.insert(20);
+        //       mpq.insert(21);
+        //       mpq.insert(4);
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(mpq.remove());
-        }
+        //       for (int i = 0; i < 5; i++) {
+        //           System.out.println(mpq.remove());
+        //       }
+
+ //       MyDeque<Integer> deque = new MyDeque<>();
+
+ //       deque.insertLeft(3);
+ //       deque.insertLeft(2);
+ //       deque.insertRight(7);
+ //       deque.insertRight(4);
+
+ //       for (int i = 0; i < 10; i++) {
+ //           System.out.println(deque.get(i));
+ //       }
+
+ //       for (int i = 0; i < 2; i++) {
+ //           System.out.println(deque.removeLeft());
+ //       }
+ //       for (int i = 0; i < 2; i++) {
+ //           System.out.println(deque.removeRight());
+ //       }
     }
 }
