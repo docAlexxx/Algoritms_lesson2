05a561a452be4d7b416e0a67a8a6dd3a6e3c992e
docAlexxx <docalex@mail.ru>
2021-10-04 19:48:55 +0400

    lesson6: HW done

--- src/main/java/lesson6/Main.java
+++ src/main/java/lesson6/Main.java
@@ -1,26 +1,51 @@
 package lesson6;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        MyTreeMap<Integer, String> map = new MyTreeMap<>();
+        Random random = new Random();
+        int TREES_QUANTITY = 20000;
+        int MAX_DEEP = 6;
+        int notBalncedQuantity = 0;
+        int nodeNumber;
 
-        map.put(5, "five");
-        map.put(4, "four");
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
+        for (int i = 0; i < TREES_QUANTITY; i++) {
+            MyTreeMap<Integer, Integer> map = new MyTreeMap<>();
+            nodeNumber = 0;
+            while (map.deep() < MAX_DEEP) {
+                nodeNumber = random.nextInt(200) - 100;
+                map.put(nodeNumber, nodeNumber);
+            }
 
-        System.out.println(map.get(4));
+            map.remove(nodeNumber);
+
+            if (!map.isBalanced()) {
+                notBalncedQuantity++;
+            }
+        }
+
+        System.out.println("процент несбалансированных деревьев - " + (double) notBalncedQuantity / TREES_QUANTITY * 100);
+
+        //       MyTreeMap<Integer, String> map = new MyTreeMap<>();
+
+        //       map.put(5, "five");
+        //       map.put(4, "four");
+        //       map.put(1, "one");
+        //       map.put(2, "two");
+        //       map.put(3, "three");
+
+        //       System.out.println(map.get(4));
 //        map.put(4, "four four");
 
-        System.out.println(map.get(4));
-        System.out.println(map.size());
+        //       System.out.println(map.get(4));
+        //       System.out.println(map.size());
 
-        System.out.println(map);
+        //       System.out.println(map);
 
-        map.remove(1);
-        System.out.println(map);
-        System.out.println(map.deep());
-        System.out.println(map.isBalanced());
+        //       map.remove(1);
+        //       System.out.println(map);
+        //       System.out.println(map.deep());
+        //       System.out.println(map.isBalanced());
     }
 }
